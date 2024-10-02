286745259caec7b2796a6ad234e7a5f9c1297c93
maulayyacyber <ridaulmaulayya@gmail.com>
2024-10-02 13:55:47 +0700

    push gan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel - CI/CD with GitHub Actions (SantriKoding.com)</title>
+        <title>Laravel - CI/CD with GitHub Actions (SantriKoding)</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
